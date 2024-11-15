59e558e0dd2d9c1f7afb78ef780ddda1c078663c
manuruiz5 <manuela.ruiz@correo.ucu.edu.uy>
2024-11-15 19:19:46 -0300

    login

--- proyectoFinal/src/pages/LoginPage/LoginPage.jsx
+++ proyectoFinal/src/pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
-import PropTypes from "prop-types"; // Importa PropTypes
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom"; // Importar useNavigate para redirigir
 
 const LoginPage = ({ onLogin }) => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [error, setError] = useState(null);
+  const navigate = useNavigate(); // Instanciar useNavigate
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await onLogin(formData);
+      // Iniciar sesión con las credenciales
+      await onLogin(formData); // Aquí no es necesario guardar 'userData'
+
+      // Redireccionar según el dominio del correo electrónico
+      const emailDomain = formData.email.split('@')[1];
+
+      if (emailDomain === "correo.ucu.edu.uy") {
+        navigate("/student"); // Redirigir al Dashboard de Estudiantes
+      } else if (emailDomain === "ucu.edu.uy") {
+        navigate("/instructor"); // Redirigir al Dashboard de Instructores
+      } else {
+        setError("Correo electrónico no válido.");
+      }
     } catch (err) {
       setError(err.message);
     }
