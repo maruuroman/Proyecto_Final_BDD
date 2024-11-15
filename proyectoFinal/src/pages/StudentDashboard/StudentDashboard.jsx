2d32c0113387933e449f713e21a1629221529ace
maruuroman <maria.roman@correo.ucu.edu.uy>
2024-11-15 10:47:45 -0300

    cambios

--- proyectoFinal/src/pages/StudentDashboard/StudentDashboard.jsx
+++ proyectoFinal/src/pages/StudentDashboard/StudentDashboard.jsx
@@ -1,17 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./StudentDashboard.module.css";
+
 
 const StudentDashboard = () => {
   const navigate = useNavigate();
+  const [studentInfo, setStudentInfo] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
       navigate("/login"); // Redirigir a login si no está autenticado
-    }
-  }, [navigate]);
-
-  return <h1>Dashboard del Alumno</h1>;
-};
-
-export default StudentDashboard;
+    } else {
+        // llamada a la API para obtener información del alumno
+        const fetchStudentInfo = async () => {
+          const response = await fetch("/api/student", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          const data = await response.json();
+          setStudentInfo(data);
+        };
+        fetchStudentInfo();
+      }
+    }, [navigate]);
+  
+    return (
+        <div className={styles.dashboardContainer}>
+        <h1>Dashboard del Alumno</h1>
+        {studentInfo ? (
+          <div>
+            <p>Nombre: {studentInfo.name}</p>
+            <p>Email: {studentInfo.email}</p>
+            
+          </div>
+        ) : (
+          <p>Cargando...</p>
+        )}
+      </div>
+    );
+  };
+  
+  export default StudentDashboard;
